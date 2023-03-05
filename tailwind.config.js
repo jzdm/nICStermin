ad50528fbca1d4e3148a2f6d8270147b208ae052
jzdm <mail@jzdm.de>
2023-03-05 14:14:08 +0100

    Frontend CSS added prefix removed tailwindcss Preflight Frontend views partially broken

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  
+  prefix: 'nit-',
+  
+  corePlugins: {
+    preflight: false,
+  },
+  
   content: [
     './public/templates/**/*.html.twig'
   ],
+  
   safelist: [
     'col-start-1', 'col-start-2', 'col-start-3', 'col-start-4', 'col-start-5', 'col-start-6', 'col-start-7',
   ],
+  
   theme: {
     extend: {},
   },
+  
   plugins: [],
+  
   variants: {
     extend: {
       display: ["group-hover"],
